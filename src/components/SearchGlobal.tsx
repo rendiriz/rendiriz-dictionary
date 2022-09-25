62f39329e22dcd7890ba4347dcf90e18be993ff2
Rendi Rizki <rendirizkir@gmail.com>
2022-09-26 01:02:10 +0700

    fix: search

--- src/components/SearchGlobal.tsx
+++ src/components/SearchGlobal.tsx
@@ -83,7 +83,7 @@ const SearchGlobal = ({ language }: TSearchGlobal) => {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <div className="fixed inset-0 bg-black bg-opacity-25" />
+              <div className="fixed inset-0 bg-black dark:bg-white bg-opacity-25 dark:bg-opacity-25" />
             </Transition.Child>
 
             <div className="fixed inset-0 overflow-y-auto">
@@ -97,7 +97,7 @@ const SearchGlobal = ({ language }: TSearchGlobal) => {
                   leaveFrom="opacity-100 scale-100"
                   leaveTo="opacity-0 scale-95"
                 >
-                  <Dialog.Panel className="w-full max-w-5xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Panel className="w-full max-w-5xl transform overflow-hidden rounded-2xl bg-white dark:bg-gray-900 p-6 text-left align-middle shadow-xl transition-all">
                     <div className="flex flex-col gap-4">
                       <input
                         type="text"
